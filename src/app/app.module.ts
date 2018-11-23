df10dfd1a188f4a7b551a18e29e04fe34d3a9cc6
Hernaldo <nano.16.rm@gmail.com>
2018-11-22 18:54:05 -0600

    Add new view(navbar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ViewClientsComponent } from './components/view-clients/view-clients.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewClientsComponent
+    ViewClientsComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
